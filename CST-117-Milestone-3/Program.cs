fe4520a72fb021bf32f83082baec6d9bdc4c3894
Rihoj <rihoj1@gmail.com>
2018-06-04 00:18:22 -0400

    This is my work - James Ray

--- CST-117-Milestone-3/Program.cs
+++ CST-117-Milestone-3/Program.cs
@@ -10,6 +10,18 @@ namespace CST_117_Milestone_3
     {
         static void Main(string[] args)
         {
+            InventoryManager manager = new InventoryManager();
+            InventoryItem inventoryItem = new InventoryItem("Test", "Hello World", 1, 2.5M, 1.5M);
+            manager.AddItem(inventoryItem);
+            Console.WriteLine("Before");
+            Console.WriteLine(manager);
+
+            InventoryItem inventoryItem2 = new InventoryItem("Test", "Hello World2", 3, 2.5M, 1.5M);
+            manager.AddItem(inventoryItem2);
+
+            Console.WriteLine("After");
+            Console.WriteLine(manager);
+            Console.ReadKey();
         }
     }
 }
